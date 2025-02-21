24c1ee286a471deb92aa7edb1da04ae54009686c
AnYenne <benhoccutephomaique@gmail.com>
2025-02-21 15:06:13 +0700

    build: install mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
 const port = 3000; // dung port la process.env.PORT khong dung dc
+const mongoose = require('mongoose');
 
+
+
+//route test
 app.get("/", (req,res)=> (res.send("hello world")) );
+
+//khởi động server
 app.listen(port, () => console.log(`example website listening on port http://localhost:${port} `));
