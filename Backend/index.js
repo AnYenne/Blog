2c62fd4643cf9eafcbb33b247928d3b0e7d9d168
AnYenne <benhoccutephomaique@gmail.com>
2025-02-24 18:59:34 +0700

    fix: move gitignore to root folder

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = 3000; // dung port la process.env.PORT khong dung dc
+const port = 3000; 
 const mongoose = require('mongoose');
 const router = require("../Backend/src/routes/index")
 //connect database
@@ -11,12 +11,8 @@ mongoose.connect(mongoURI)
 .then(() => console.log("mongoDB connected"))
 .catch(err => console.error("mongoDB connect error", err))
 
-// //route test
-app.get("/", (req,res)=> (res.send("hello world")) );
-console.log("MongoDB URI:", mongoose.connection.host + "/" + mongoose.connection.name)
-
+//route
 router(app)
-
 
 //khởi động server
 app.listen(port, () => console.log(`example website listening on port http://localhost:${port} `));
