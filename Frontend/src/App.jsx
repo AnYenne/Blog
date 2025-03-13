d19867e86c90fb002c6a94aa8f301f2313bdd1b3
AnYenne <benhoccutephomaique@gmail.com>
2025-03-13 22:03:44 +0700

    feat: ui build footer component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import { Container } from '@mui/material'
 import Header from './components/Header'
+import Homepage from './components/Body/Homepage'
 
 function App() {
 
   return (
     <div className='App'>
       <Header></Header>
-      <Container>
-      </Container>
+      <Homepage></Homepage>
     </div>  
   )
 }
---
+++ Frontend/src/components/Footer/index.jsx
@@ -0,0 +1,41 @@
+import { Box, Icon, Input, Stack, Typography, Button } from "@mui/material"
+
+
+const Footer = () => {
+    
+    return(
+        <Box sx={{display: 'flex', flexDirection:{xs:'column', md:'row'}, justifyContent:'space-around', alignItems:'center', paddingBottom:'24px', paddingLeft:{xs:'24px'},  fontFamily:'Nunito', fontWeight:'600'}}>
+            <Box sx={{display:'flex', flexDirection:{xs:'column', md:'column'}}}>
+                <Box  sx={{paddingBottom: {xs:'24px'},paddingLeft:{xs:'40px', md:'none'}, paddingRight:{xs: '40px', md:'none'}}}>
+                    <Typography>Connect with us</Typography>
+                    <Stack direction='row' spacing={4} >
+                        <Button variant="text" href="#">
+                            <Icon sx={{color: '#060842'}}>facebook</Icon>
+                        </Button>
+                        <Button variant="text" href="#">
+                            <Icon sx={{color: '#060842'}}>email</Icon>
+                        </Button>
+                        <Button variant="text" href="#">
+                            <Icon sx={{color: '#060842'}}>phone</Icon>
+                        </Button>
+                    </Stack>
+                </Box>
+                <Box>
+                    <Typography>CÔNG TY TNMTV ANN</Typography>
+                    <Typography>Người đại diện: Nguyen Thi Ngoc Uyen</Typography>
+                </Box>
+            </Box>
+            <Box>
+                <Typography sx={{paddingTop: {xs:'24px', md:'none'}, paddingBottom:{xs:"12px"}}}>
+                STAY UPDATED WITH MORICO CONTEMPORARY JAPANESE LIFESTYLE
+                </Typography>
+                <Typography sx={{paddingBottom: '24px'}}>
+                Sign up for information, new menu, catering, horeca,…
+                </Typography>
+                <Input placeholder="your@email" sx={{backgroundColor:'#999', color:'#FFF',padding:{xs:'0px 14px'},borderRadius:'4px', fontFamily:'Nunito', fontWeight:'600', width:{xs:'300px'}}} />
+                <Button variant="contained" sx={{backgroundColor:'#060842', color:'#FFA500', fontFamily:'Nunito', fontWeight:'600', width:'160px', margin:{xs:"12px auto", md:'0px 12px'}}}>SIGN UP</Button>
+            </Box>
+        </Box>    
+    )
+}
+export default Footer
